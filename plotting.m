0a4a3750a41bd7b7fd70127336147828b04967c7
Tomas-Homola <homola.tom6@gmail.com>
2022-02-09 01:19:04 +0100

    Ďalší kód na vizualizáciu

--- plotting.m
+++ plotting.m
@@ -1,5 +1,6 @@
 %% Nacitanie dat
 data100 = readmatrix('data100.csv');
+data100_def = readmatrix('data100_def.csv');
 data1000 = readmatrix('data1000.csv');
 
 %% Zistenie poctu trajektorii
@@ -38,4 +39,45 @@ for n = 2:count
 		'-', 'LineWidth', 1);
 end
 plot(data1000(1, 1), data1000(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
+hold off
+
+%% Trajektorie z DEFINICIE
+%% 100 trajektorii
+%% Zistenie poctu trajektorii
+data100_def = readmatrix('data100_def.csv');
+[count, ~] = size(data100_def);
+count = count - 1; % minus prvy riadok, kde je ulozena casova os
+
+%% Vizualizacia
+figure('Name', '100 trajectories', 'NumberTitle', 'off');
+title('100 trajectories of Ito process')
+xlabel('Time', 'FontSize', 20)
+ylabel('Angle θ', 'FontSize', 20)
+% ytickformat('%.0f°')
+hold on
+for n = 2:count
+	plot(data100_def(1, :), data100_def(n, :), ...
+		'-', 'LineWidth', 0.5);
+end
+plot(data100_def(1, 1), data100_def(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
+hold off
+
+%% 1000 trajektorii
+%% Zistenie poctu trajektorii
+data1000_def = readmatrix('data1000_def.csv');
+[count, ~] = size(data1000_def);
+count = count - 1; % minus prvy riadok, kde je ulozena casova os
+
+%% Vizualizacia
+figure('Name', '1000 trajectories', 'NumberTitle', 'off');
+title('1000 trajectories of Ito process')
+xlabel('Time', 'FontSize', 20)
+ylabel('Angle θ', 'FontSize', 20)
+% ytickformat('%.0f°')
+hold on
+for n = 2:count
+	plot(data1000_def(1, 1:end-1), data1000_def(n, 1:end-1), ...
+		'-', 'LineWidth', 0.5);
+end
+plot(data1000_def(1, 1), data1000_def(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
 hold off
