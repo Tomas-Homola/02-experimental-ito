3412561a6c9c7887443aedab4750af2732404608
Tomas-Homola <homola.tom6@gmail.com>
2022-02-15 15:07:07 +0100

    Drobná úprava na kreslenie data100_def

--- plotting.m
+++ plotting.m
@@ -1,6 +1,5 @@
 %% Nacitanie dat
 data100 = readmatrix('data100.csv');
-data100_def = readmatrix('data100_def.csv');
 data1000 = readmatrix('data1000.csv');
 
 %% Zistenie poctu trajektorii
@@ -62,7 +61,27 @@ end
 plot(data100_def(1, 1), data100_def(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
 hold off
 
-%% 1000 trajektorii
+%% 100 trajektorii cez definiciu
+%% Zistenie poctu trajektorii
+data100_def = readmatrix('data100_def.csv');
+[count, ~] = size(data100_def);
+count = count - 1; % minus prvy riadok, kde je ulozena casova os
+
+%% Vizualizacia
+figure('Name', '100 trajectories', 'NumberTitle', 'off');
+title('100 trajectories of Ito process')
+xlabel('Time', 'FontSize', 20)
+ylabel('Angle θ', 'FontSize', 20)
+% ytickformat('%.0f°')
+hold on
+for n = 2:count
+	plot(data100_def(1, :), data100_def(n, :), ...
+		'-', 'LineWidth', 0.5);
+end
+plot(data100_def(1, 1), data100_def(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
+hold off
+
+%% 1000 trajektorii cez definiciu
 %% Zistenie poctu trajektorii
 data1000_def = readmatrix('data1000_def.csv');
 [count, ~] = size(data1000_def);
