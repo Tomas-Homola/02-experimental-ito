9ab12c5ae0ea25b7d02c786f30d4bc2b9d0b2028
Tomas-Homola <homola.tom6@gmail.com>
2022-02-17 17:30:01 +0100

    Pár kozmetických úprav, stačí už iba celý skript pustit na raz

--- plotting.m
+++ plotting.m
@@ -1,43 +1,38 @@
-%% Nacitanie dat
-data100 = readmatrix('data100.csv');
-data1000 = readmatrix('data1000.csv');
+%% 100 trajektorii cez transformaciu
+data100_transf = readmatrix('data100_transf.csv');
 
-%% Zistenie poctu trajektorii
-[count, ~] = size(data100);
-count = count - 1; % minus prvy riadok, kde je ulozena casova os
+% Zistenie poctu trajektorii
+[count, ~] = size(data100_transf);
 
-%% Vizualizacia
+% Vizualizacia
 figure('Name', '100 trajectories', 'NumberTitle', 'off');
-title('100 trajectories of Ito process')
+title('100 trajectories of Ito process by transform')
 xlabel('Time', 'FontSize', 20)
 ylabel('Angle θ', 'FontSize', 20)
-yticks(-45:15:80)
-ytickformat('%.0f°')
 hold on
 for n = 2:count
-	plot(data100(1, :), data100(n, :), ...
+	plot(data100_transf(1, :), data100_transf(n, :), ...
 		'-', 'LineWidth', 0.5);
 end
-plot(data100(1, 1), data100(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
+plot(data100_transf(1, 1), data100_transf(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
 hold off
 
-%% Zistenie poctu trajektorii
-[count, ~] = size(data1000);
-count = count - 1; % minus prvy riadok, kde je ulozena casova os
+%% 1000 trajektorii cez transformaciu
+data1000_transf = readmatrix('data1000_transf.csv');
+% Zistenie poctu trajektorii
+[count, ~] = size(data1000_transf);
 
-%% Vizualizacia
+% Vizualizacia
 figure('Name', '1000 trajectories', 'NumberTitle', 'off');
-title('1000 trajectories of Ito process')
+title('1000 trajectories of Ito process by transform')
 xlabel('Time', 'FontSize', 20)
 ylabel('Angle θ', 'FontSize', 20)
-ytickformat('%.0f°')
-yticks(-45:15:80)
 hold on
 for n = 2:count
-	plot(data1000(1, :), data1000(n, :), ...
+	plot(data1000_transf(1, :), data1000_transf(n, :), ...
 		'-', 'LineWidth', 1);
 end
-plot(data1000(1, 1), data1000(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
+plot(data1000_transf(1, 1), data1000_transf(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
 hold off
 
 %% Trajektorie z DEFINICIE
@@ -45,34 +40,12 @@ hold off
 %% Zistenie poctu trajektorii
 data100_def = readmatrix('data100_def.csv');
 [count, ~] = size(data100_def);
-count = count - 1; % minus prvy riadok, kde je ulozena casova os
 
-%% Vizualizacia
+% Vizualizacia
 figure('Name', '100 trajectories', 'NumberTitle', 'off');
-title('100 trajectories of Ito process')
-xlabel('Time', 'FontSize', 20)
-ylabel('Angle θ', 'FontSize', 20)
-% ytickformat('%.0f°')
-hold on
-for n = 2:count
-	plot(data100_def(1, :), data100_def(n, :), ...
-		'-', 'LineWidth', 0.5);
-end
-plot(data100_def(1, 1), data100_def(2, 1), '.', 'Color', 'red', 'MarkerSize', 25);
-hold off
-
-%% 100 trajektorii cez definiciu
-%% Zistenie poctu trajektorii
-data100_def = readmatrix('data100_def.csv');
-[count, ~] = size(data100_def);
-count = count - 1; % minus prvy riadok, kde je ulozena casova os
-
-%% Vizualizacia
-figure('Name', '100 trajectories definition', 'NumberTitle', 'off');
 title('100 trajectories of Ito process by definition')
 xlabel('Time', 'FontSize', 20)
 ylabel('Angle θ', 'FontSize', 20)
-% ytickformat('%.0f°')
 hold on
 for n = 2:count
 	plot(data100_def(1, :), data100_def(n, :), ...
@@ -85,14 +58,12 @@ hold off
 %% Zistenie poctu trajektorii
 data1000_def = readmatrix('data1000_def.csv');
 [count, ~] = size(data1000_def);
-count = count - 1; % minus prvy riadok, kde je ulozena casova os
 
-%% Vizualizacia
+% Vizualizacia
 figure('Name', '1000 trajectories definition', 'NumberTitle', 'off');
 title('1000 trajectories of Ito process by definition')
 xlabel('Time', 'FontSize', 20)
 ylabel('Angle θ', 'FontSize', 20)
-% ytickformat('%.0f°')
 hold on
 for n = 2:count
 	plot(data1000_def(1, 1:end-1), data1000_def(n, 1:end-1), ...
